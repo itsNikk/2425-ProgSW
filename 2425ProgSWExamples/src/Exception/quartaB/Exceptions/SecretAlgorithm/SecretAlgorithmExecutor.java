f4f99f7b8679a5fc40a6cb71d02a0d04fb1e4fef
iTzNikk <nicolo.buganza@tiscali.it>
2025-03-28 00:31:26 +0100

    Update SecretAlgorithmExecutor.java

--- 2425ProgSWExamples/src/Exception/quartaB/Exceptions/SecretAlgorithm/SecretAlgorithmExecutor.java
+++ 2425ProgSWExamples/src/Exception/quartaB/Exceptions/SecretAlgorithm/SecretAlgorithmExecutor.java
@@ -2,6 +2,7 @@ package Exception.quartaB.Exceptions.SecretAlgorithm;
 
 public class SecretAlgorithmExecutor {
     private DataContainer dataContainer;
+    private final int SIM_SPEED = 5;
 
     public SecretAlgorithmExecutor(int size) {
         dataContainer = new DataContainer(size);
@@ -9,22 +10,25 @@ public class SecretAlgorithmExecutor {
 
     public void executeAlgorithm(int iterations) throws EmptyListException, DataCorruptionException, CannotRemoveElementException, CriticalDataFailureException, InterruptedException {
         for (int i = 0; i < iterations; i++) {
-            Thread.sleep(1000);
-            System.out.println("SAlgo ITER " + (i + 1));
+            Thread.sleep(SIM_SPEED);
+            System.out.println("S.E.C.R.E.T. A.L.G.O. ITER " + (i + 1));
             double prob = Math.random();
 
-            if (prob < 0.3) {
-                System.out.println("Scambio elementi!");
-                dataContainer.swapElements();
-            } else if (prob < 0.8) {
+            if (prob < 0.6) {
                 System.out.println("Corruption initiated...");
                 dataContainer.corruptData();
-            } else if (prob < 0.99) {
-                System.out.println("Random event removal intiated...");
-                dataContainer.removeRandomElement();
-            } else {
-                System.out.println("Corruption state check initiated...");
+            } else if (prob < 0.9) {
+                System.out.println("Scambio elementi!");
+                dataContainer.swapElements();
+            }
+
+            double p2 = Math.random();
+            if (p2 < 0.7) {
+                System.out.println("Check corruption state initiated..");
                 dataContainer.resetIfCorrupted();
+            } else {
+                System.out.println("Random rem initiated..");
+                dataContainer.removeRandomElement();
             }
 
             dataContainer.getInfo();
